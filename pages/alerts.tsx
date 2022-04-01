39caf5b901bb9c48e0f67c58743afd49da9c54b6
VictorMadu <ebubevm@gmail.com>
2022-04-01 07:22:13 +0100

    feat: Add alerts page

---
+++ pages/alerts.tsx
@@ -0,0 +1,9 @@
+import React, { useEffect } from "react";
+import type { NextPage } from "next";
+import AlertsLayout from "../components/alerts-layout";
+
+const Home: NextPage = () => {
+  return <AlertsLayout />;
+};
+
+export default Home;
