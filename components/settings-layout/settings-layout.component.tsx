f491ef0f9b7c2fc49aa13c70fec6463fd2318d66
VictorMadu <ebubevm@gmail.com>
2022-04-01 14:19:22 +0100

    feat: Remove unnecessary code from settings layout
    Contained code for notification layout so this replaces it will just a simple text

--- components/settings-layout/settings-layout.component.tsx
+++ components/settings-layout/settings-layout.component.tsx
@@ -1,27 +1,11 @@
 import React from "react";
 import Layout from "../layout";
 import * as styles from "./settings-layout.styles";
-import map from "lodash/map";
-import range from "lodash/range";
 
 const Settings = () => {
   return (
     <Layout showNav={true}>
-      <div className={styles.container()}>
-        <div className={styles.notificationList()}>
-          {map(range(0, 10), (id) => (
-            <div className={styles.notification()}>
-              <p className={styles.notificationText()}>
-                {"USD/NGN has passed the 450.56 level"}
-              </p>
-              <p className={styles.notificationTime()}>
-                <span>{"12/05/2022"}</span>
-                <span>{"12:30pm"}</span>
-              </p>
-            </div>
-          ))}
-        </div>
-      </div>
+      <div className={styles.container()}>Settings Layout</div>
     </Layout>
   );
 };
--- components/settings-layout/settings-layout.styles.ts
+++ components/settings-layout/settings-layout.styles.ts
@@ -10,21 +10,3 @@ export const container = () =>
     "flex",
     "flex-col"
   );
-
-export const notificationList = () =>
-  classNames(
-    "mx-auto",
-    "w-full",
-    "rounded",
-    "shadow-[0px_0px_2px_0px_rgba(0,0,0,0.3)]",
-    "divide-y-2",
-    "divide-neu-400/20",
-    "overflow-auto"
-  );
-
-export const notification = () => classNames("px-4", "py-4", "space-y-1");
-
-export const notificationText = () => classNames();
-
-export const notificationTime = () =>
-  classNames("text-sm", "text-neu-600/90", "text-right");
