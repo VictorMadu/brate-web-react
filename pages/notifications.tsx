b443b58bc4aaa466179d066dfc43c852fc5b8070
VictorMadu <ebubevm@gmail.com>
2022-04-01 07:23:18 +0100

    feat: Add notification page

---
+++ pages/notifications.tsx
@@ -0,0 +1,9 @@
+import React, { useEffect } from "react";
+import type { NextPage } from "next";
+import HomeLayout from "../components/home-layout";
+
+const Notification: NextPage = () => {
+  return <div>Notification Page</div>;
+};
+
+export default Notification;
