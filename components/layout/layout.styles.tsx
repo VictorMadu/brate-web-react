f69fad068cf6381a5aabaf1bbb222a84af485d77
VictorMadu <ebubevm@gmail.com>
2022-04-01 07:21:44 +0100

    feat: Improve layout styling

--- components/layout/layout.styles.tsx
+++ components/layout/layout.styles.tsx
@@ -6,7 +6,7 @@ export const container = () =>
       "grid",
       "grid-rows-[1fr_auto]",
       "md:grid-rows-[auto_1fr]",
-      "min-h-screen",
+      "h-screen",
       "before:box-border",
       "after:box-border",
       "text-neu-800/90",
@@ -18,4 +18,4 @@ export const navContainer = () =>
   new CNG().values("row-[2/-1] md:row-[1/2]").generate([]);
 
 export const childrenContainer = () =>
-  new CNG().values("row-[1/2] md:row-[2/-1]").generate([]);
+  new CNG().values("row-[1/2] md:row-[2/-1]", "overflow-auto").generate([]);
