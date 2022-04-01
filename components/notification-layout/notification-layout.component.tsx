46f97808fecef879304c3dd67cabb59eb6a2dfa3
VictorMadu <ebubevm@gmail.com>
2022-04-01 14:25:36 +0100

    feat: Modified notification layout UI
    Remove the overlap between the scrollbar and notification list border

--- components/notification-layout/notification-layout.component.tsx
+++ components/notification-layout/notification-layout.component.tsx
@@ -8,18 +8,20 @@ const NotificationsLayout = () => {
   return (
     <Layout showNav={true}>
       <div className={styles.container()}>
-        <div className={styles.notificationList()}>
-          {map(range(0, 10), (id) => (
-            <div className={styles.notification()}>
-              <p className={styles.notificationText()}>
-                {"USD/NGN has passed the 450.56 level"}
-              </p>
-              <p className={styles.notificationTime()}>
-                <span>{"12/05/2022"}</span>
-                <span>{"12:30pm"}</span>
-              </p>
-            </div>
-          ))}
+        <div className={styles.notificationContainer()}>
+          <div className={styles.notificationList()}>
+            {map(range(0, 10), (id) => (
+              <div className={styles.notification()}>
+                <p className={styles.notificationText()}>
+                  {"USD/NGN has passed the 450.56 level"}
+                </p>
+                <p className={styles.notificationTime()}>
+                  <span>{"12/05/2022"}</span>
+                  <span>{"12:30pm"}</span>
+                </p>
+              </div>
+            ))}
+          </div>
         </div>
       </div>
     </Layout>
--- components/notification-layout/notification-layout.styles.ts
+++ components/notification-layout/notification-layout.styles.ts
@@ -11,15 +11,18 @@ export const container = () =>
     "flex-col"
   );
 
+export const notificationContainer = () =>
+  classNames("overflow-auto", "mx-auto", "w-full");
+
 export const notificationList = () =>
   classNames(
-    "mx-auto",
-    "w-full",
+    "mr-0.5",
+    "border",
+    "border-neu-400/20",
     "rounded",
     "shadow-[0px_0px_2px_0px_rgba(0,0,0,0.3)]",
     "divide-y-2",
-    "divide-neu-400/20",
-    "overflow-auto"
+    "divide-neu-400/20"
   );
 
 export const notification = () => classNames("px-4", "py-4", "space-y-1");
@@ -27,4 +30,4 @@ export const notification = () => classNames("px-4", "py-4", "space-y-1");
 export const notificationText = () => classNames();
 
 export const notificationTime = () =>
-  classNames("text-sm", "text-neu-600/90", "text-right");
+  classNames("text-sm", "text-neu-500/90", "text-right");
