b406790fafd68d2acefc5d7ea85f654da43c2997
VictorMadu <ebubevm@gmail.com>
2022-04-01 12:25:45 +0100

    feat: Improve Nav UI

--- components/layout/nav/nav.styles.ts
+++ components/layout/nav/nav.styles.ts
@@ -2,13 +2,40 @@ import { ClassNameGenerator as CNG } from "../../../utils";
 
 export const container = () =>
   new CNG()
-    .values("flex", "bg-blue-600", "md:px-[10%]", "items-center")
+    .values(
+      "flex",
+      "md:flex-col",
+      "bg-blue-600",
+      "items-center md:items-start",
+      "md:h-full",
+      "divide-y",
+      "divide-neu-100/50"
+    )
     .generate([]);
 
 export const tilte = () =>
-  new CNG().values("hidden", "md:block", "grow").generate([]);
+  new CNG()
+    .values(
+      "hidden",
+      "md:block",
+      "text-neu-50",
+      "font-bold",
+      "text-3xl",
+      "md:self-center",
+      "md:py-2"
+    )
+    .generate([]);
 
 export const navContainer = () =>
   new CNG()
-    .values("grow", "md:grow-0", "flex", "justify-around", "md:gap-x-4")
+    .values(
+      "grow",
+      "md:grow-0",
+      "flex",
+      "justify-around",
+      "md:gap-x-4",
+      "md:flex-col",
+      "md:pr-8",
+      "md:pt-6"
+    )
     .generate([]);
