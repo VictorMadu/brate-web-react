d9dc805e2b01c5767d637ab55231d8ae858b4ad1
VictorMadu <ebubevm@gmail.com>
2022-04-01 07:21:23 +0100

    feat: Update home page layout
    Adds pagination
    Adds base currency selection dropdown
    Improved styling

--- components/home-layout/home-layout.component.tsx
+++ components/home-layout/home-layout.component.tsx
@@ -39,6 +39,15 @@ const HomeLayout = () => {
                 onChange={() => {}}
                 disabled={false}
               />
+              <div className={styles.dropdownContainer(true)}>
+                <ul>
+                  {map(range(0, 20), (id) => (
+                    <li key={id} className={styles.dropItem(false)}>
+                      {"NGN"}
+                    </li>
+                  ))}
+                </ul>
+              </div>
             </div>
           </div>
 
@@ -48,24 +57,30 @@ const HomeLayout = () => {
           </ul>
         </div>
 
-        <table className={styles.pricesTable()}>
-          <thead className={styles.pricesTableHeadRow()}>
-            <th className={styles.pricesTableHeadCol1()}>Currency Pair</th>
-            <th className={styles.pricesTableHeadCol2()}>Price</th>
-            <th className={styles.pricesTableHeadCol3()}>24H Change (%)</th>
-            <th className={styles.pricesTableHeadCol4()}></th>
-          </thead>
-          <tbody className={styles.pricesTableBody()}>
-            {map(range(0, 20), (id) => (
-              <tr key={id} className={styles.pricesTableBodyRow()}>
-                <td className={styles.pricesTableBodyCol1()}>USD/NGN</td>
-                <td className={styles.pricesTableBodyCol2()}>450.345939</td>
-                <td className={styles.pricesTableBodyCol3()}>-1.38</td>
-                <td className={styles.pricesTableBodyCol4()}>Star</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
+        <div className={styles.paginationContainer()}>
+          <span>1</span> - <span>10</span> of <span>100</span>
+        </div>
+
+        <div className={styles.pricesTableContainer()}>
+          <table className={styles.pricesTable()}>
+            <thead className={styles.pricesTableHeadRow()}>
+              <th className={styles.pricesTableHeadCol1()}>Currency Pair</th>
+              <th className={styles.pricesTableHeadCol2()}>Price</th>
+              <th className={styles.pricesTableHeadCol3()}>24H Change (%)</th>
+              <th className={styles.pricesTableHeadCol4()}></th>
+            </thead>
+            <tbody className={styles.pricesTableBody()}>
+              {map(range(0, 10), (id) => (
+                <tr key={id} className={styles.pricesTableBodyRow()}>
+                  <td className={styles.pricesTableBodyCol1()}>USD/NGN</td>
+                  <td className={styles.pricesTableBodyCol2()}>450.345939</td>
+                  <td className={styles.pricesTableBodyCol3()}>-1.38</td>
+                  <td className={styles.pricesTableBodyCol4()}>Star</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
       </div>
     </Layout>
   );
--- components/home-layout/home-layout.styles.ts
+++ components/home-layout/home-layout.styles.ts
@@ -1,21 +1,59 @@
 import classNames from "classnames";
 
 export const container = () =>
-  classNames("w-full", "px-[4%]", "md:px-[10%]", "py-3");
+  classNames(
+    "w-full",
+    "px-[4%]",
+    "md:px-[10%]",
+    "py-3",
+    "h-full",
+    "flex",
+    "flex-col"
+  );
 
-export const switchContainer = () => classNames("flex", "justify-end");
+export const switchContainer = () =>
+  classNames("flex", "justify-end", "shrink-0");
 
 export const settingContainer = () =>
-  classNames("flex", "justify-between", "pt-10");
+  classNames("flex", "justify-between", "pt-10", "shrink-0");
 
-export const baseContainer = () =>
-  classNames("flex", "rounded", "overflow-hidden");
+export const baseContainer = () => classNames("flex", "rounded");
 
-export const base = () => classNames("bg-neu-400", "px-2", "py-1.5");
+export const base = () =>
+  classNames("bg-neu-400", "px-2", "py-1.5", "rounded-l");
 
-export const inputContainer = () => classNames("flex");
+export const inputContainer = () => classNames("relative", "flex");
 
-export const input = () => classNames("max-w-[6rem]");
+export const input = () =>
+  classNames("max-w-[5rem]", "focus:outline-none", "bg-neu-200", "rounded-r");
+
+export const dropdownContainer = (isShow: boolean) =>
+  classNames(
+    "top-full",
+    "left-0",
+    "bg-neu-50 dark:bg-neu-900",
+    "max-h-[10rem]",
+    "overflow-auto",
+    "rounded",
+    "[--shadow:0px_0px_6px_4px_rgba(0,0,0,0.2)] dark:[--shadow:0px_0px_10px_4px_rgba(255,255,255,0.05)] [box-shadow:var(--shadow)]",
+    "absolute",
+    "cursor-pointer",
+    isShow ? "absolute" : "hidden"
+  );
+
+export const dropItem = (isActive: boolean) =>
+  classNames(
+    "px-5",
+    "py-2",
+    "flex",
+    "gap-x-2",
+    "w-full",
+    "items-center",
+    "transition-all",
+    "hover:bg-neu-200 focus:bg-neu-200 dark:hover:bg-neu-800 dark:focus:bg-neu-800",
+    { "text-pri-700 dark:text-pri-200": isActive },
+    { "font-semibold": isActive }
+  );
 
 export const filterContainer = () =>
   classNames("flex", "gap-x-3", "items-end", "font-bold");
@@ -24,13 +62,19 @@ export const filterLeft = () => classNames("flex");
 
 export const filterRight = () => classNames("flex");
 
+export const paginationContainer = () =>
+  classNames("mt-8", "text-right", "shrink-0", "overflow-auto");
+
+export const pricesTableContainer = () =>
+  classNames("overflow-auto", "h-0", "grow", "rounded-md");
+
 export const pricesTable = () =>
   classNames(
     "w-full",
     "shadow-[0px_0px_6px_rgba(0,0,0,0.4)]",
-    "mt-8",
-    "rounded-md",
-    "overflow-hidden"
+    "mt-4",
+    "overflow-hidden",
+    "rounded-md"
   );
 
 export const pricesTableHeadRow = () =>
@@ -59,7 +103,7 @@ export const pricesTableBody = () => classNames();
 export const pricesTableBodyRow = () =>
   classNames("even:bg-neu-200", "hover:bg-neu-300");
 
-export const pricesTableBodyCol = () => classNames("px-3", "py-2");
+export const pricesTableBodyCol = () => classNames("px-3", "py-3");
 
 export const pricesTableBodyCol1 = () =>
   classNames(pricesTableBodyCol(), "w-5/12");
