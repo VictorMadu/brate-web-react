c1b7b1099d1d2cf15517c6f7e23e7050101942dc
VictorMadu <ebubevm@gmail.com>
2022-04-02 11:56:17 +0100

    feat: Add wallet page UI

---
+++ components/wallet-layout/wallet-layout.component.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const WalletLayout = () => {
+  return <div>WalletLayout</div>;
+};
+
+export default WalletLayout;
---
+++ pages/settings/wallet.tsx
@@ -0,0 +1,9 @@
+import { NextPage } from "next";
+import React from "react";
+import WalletLayout from "../../components/wallet-layout";
+
+const Wallet: NextPage = () => {
+  return <WalletLayout />;
+};
+
+export default Wallet;
