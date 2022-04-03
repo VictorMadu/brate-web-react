84e762965ce23801cd7388f4fc0390d25007513b
VictorMadu <ebubevm@gmail.com>
2022-04-03 20:39:09 +0100

    feat: Improved UI

--- components/display-layout/display-layout.component.tsx
+++ components/display-layout/display-layout.component.tsx
@@ -1,5 +1,4 @@
-import React from "react";
-import Switch from "../../core/switch";
+import React, { useEffect, useRef, useState } from "react";
 import app from "../../language/en/app";
 import Layout from "../layout";
 import * as styles from "./display-layout.styles";
@@ -7,19 +6,35 @@ import * as styles from "./display-layout.styles";
 const display = app.settings.display;
 
 const DisplayLayout = () => {
+  const isOnRef = useRef(false);
+  const switchBtnRef = useRef<HTMLButtonElement>(null);
+  const switchBtnContainerRef = useRef<HTMLDivElement>(null);
+  // useEffect(() => {
+  //   const id = setInterval(() => {
+  //     if (!switchBtnRef.current || !switchBtnContainerRef.current) return;
+  //     styles.toggleSwitchBtn(
+  //       switchBtnRef.current,
+  //       switchBtnContainerRef.current,
+  //       isOnRef.current
+  //     );
+  //     isOnRef.current = !isOnRef.current;
+  //   }, 2500);
+
+  //   return () => clearInterval(id);
+  // }, []);
+
+  useEffect(() => {
+    console.log("Mounting or Re-rendering");
+  });
+
   return (
     <Layout showNav={true}>
       <div className={styles.container()}>
         <div className={styles.option()}>
           <p>{display.dark_mode}</p>
-          <Switch
-            onText={"Light"}
-            offText={"Dark"}
-            onClick={() => {
-              alert("TODO");
-            }}
-            isOn={false}
-          />
+          <div className={styles.switchContainer()} ref={switchBtnContainerRef}>
+            <button className={styles.switchBtn()} ref={switchBtnRef}></button>
+          </div>
         </div>
       </div>
     </Layout>
--- components/display-layout/display-layout.styles.ts
+++ components/display-layout/display-layout.styles.ts
@@ -16,7 +16,38 @@ export const option = () =>
     "flex",
     "justify-between",
     "items-center",
-    "pb-2",
-    "border-b",
-    "border-neu-400/30"
+    "border",
+    "border-neu-400/30",
+    "rounded-md",
+    "px-3",
+    "py-4"
   );
+
+export const switchContainer = () =>
+  classNames("relative", "rounded-xl", "bg-neu-300/90", "w-9", "h-5");
+
+export const switchBtn = () =>
+  classNames(
+    "w-5",
+    "h-5",
+    "rounded-full",
+    "bg-neu-500/90",
+    // "block",
+    "absolute",
+    "top-0",
+    "transition-all",
+    "duration-1000"
+  );
+
+const toggleSwitch = () => classNames("right-0", "bg-pri-500/90");
+const toggleSwitchContainer = () => classNames("bg-pri-200/60");
+
+export const toggleSwitchBtn = (
+  switchBtn: HTMLButtonElement,
+  switchBtnContainer: HTMLDivElement,
+  isOn: boolean
+) => {
+  const action = isOn ? "add" : "remove";
+  switchBtn.classList[action](...toggleSwitch().split(" "));
+  switchBtnContainer.classList[action](...toggleSwitchContainer().split(" "));
+};
