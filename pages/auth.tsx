521a631456f2fb899623fffe87cf724fc9a57372
VictorMadu <ebubevm@gmail.com>
2022-04-05 21:13:34 +0100

    feat: Create auth page
    Creates auth page and connects auth layout UI to it

---
+++ pages/auth.tsx
@@ -0,0 +1,9 @@
+import { NextPage } from "next";
+import React from "react";
+import AuthLayout from "../components/auth-layout";
+
+const Auth: NextPage = () => {
+  return <AuthLayout />;
+};
+
+export default Auth;
