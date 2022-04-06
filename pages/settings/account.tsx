56c921a996a7391120502e53705a87940ce163e2
VictorMadu <ebubevm@gmail.com>
2022-04-06 14:40:41 +0100

    feat: Add account settings page for cc4be5a UI

---
+++ pages/settings/account.tsx
@@ -0,0 +1,13 @@
+import { NextPage } from "next";
+import React from "react";
+import AccountSettingsLayout from "../../components/account-settings-layout";
+
+const Account: NextPage = () => {
+  return (
+    <div>
+      <AccountSettingsLayout />
+    </div>
+  );
+};
+
+export default Account;
