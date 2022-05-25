a21dc15f763d5192214c83afaf0062301db50d3b
VictorMadu <ebubevm@gmail.com>
2022-05-25 08:44:37 +0100

    feat: Add index file and remove folder
    - Add index file to export http route call functions
    - user_update_check_time folder was renamed to user-update-check-time . Check bfadeacde787025b98aa04cc95f50c37581184be

--- api/http/user_update_last_check_time/types/index.ts
+++
@@ -1,8 +0,0 @@
-import { FromSchema } from "json-schema-to-ts";
-import { headerSchema } from "./schema/header.schema";
-import { bodySchema } from "./schema/body.schema";
-import { res2xxSchema } from "./schema/response.schema";
-
-export type Headers = FromSchema<typeof headerSchema>;
-export type Body = FromSchema<typeof bodySchema>;
-export type Res = FromSchema<typeof res2xxSchema>;
--- api/http/user_update_last_check_time/types/schema/body.schema.ts
+++
@@ -1,8 +0,0 @@
-export const bodySchema = {
-  type: "object",
-  properties: {
-    last_check_time: { type: "number" },
-  },
-  required: ["last_check_time"],
-  additionalProperties: false,
-} as const;
