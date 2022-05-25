9124e0d22f9ea29a7393c6e399f12f6a8c614f1a
VictorMadu <ebubevm@gmail.com>
2022-05-25 08:42:00 +0100

    feat: Add wallet currency withdrawal route call function

---
+++ api/http/wallet-withdraw/index.ts
@@ -0,0 +1,23 @@
+import { WITHDRAW_FROM_WALLET } from "../_constants/routes";
+import { callHtppServer, createAuthHeader } from "../_utils";
+import { noTokenError } from "../_utils/no-token-error";
+import { Headers, Body, Res } from "./types";
+
+interface InData {
+  token?: string;
+  currency: string;
+  amount: number;
+}
+
+export const withdrawFromWallet = async (inData: InData) => {
+  if (!inData.token) return noTokenError();
+  const headers: Headers = { authorization: createAuthHeader(inData.token) };
+
+  const data: Body = { currency: inData.currency, amount: inData.amount };
+  return await callHtppServer<Res>({
+    url: WITHDRAW_FROM_WALLET,
+    method: "post",
+    headers,
+    data,
+  });
+};
