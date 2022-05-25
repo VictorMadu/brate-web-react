09e56b6556acb74337636f085a994414cd3ce24f
VictorMadu <ebubevm@gmail.com>
2022-05-25 08:08:49 +0100

    Add sign up otp route call function

---
+++ api/http/user-send-otp/types/schema/response.schema.ts
@@ -0,0 +1,11 @@
+import { FromSchema } from "json-schema-to-ts";
+
+export const resxxxSchema = {
+  type: "object",
+  properties: {
+    status: { type: "boolean" },
+    msg: { type: "string" },
+  },
+  required: ["status", "msg"],
+  additionalProperties: false,
+} as const;
